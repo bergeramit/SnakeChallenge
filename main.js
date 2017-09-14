0065f7179a94170097a5c17d971012de557ec5ac
עמית ברגר <mytbrgr@h-MBP-sl-myt.home>
2017-09-14 12:17:00 +0300

    added the board

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 
+let ROWS = 20;
+let COLS = 50;
+
+
+initiateBoard();
+
+
+
+
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -10,6 +19,22 @@ function sleep(ms) {
   }
 
   //demo();
+var initiateBoard = function(){
+    let board = document.getElementById("board");
+    for(let i=0;i<ROWS;i++){
+        let ul = document.createElement("ul");
+        ul.setAttribute("id",i)
+        for(let j=0;j<COLS;++j){
+            let li = document.createElement("li");
+            li.setAttribute("id",j);
+            ul.appendChild(li);
+        }
+        board.appendChild(ul);
+    }
+}
+
+
+
 
 
 
