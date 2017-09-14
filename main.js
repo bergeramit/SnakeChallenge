0cba339a42e4b6116edc0cb16a7af73cb7328e8c
עמית ברגר <mytbrgr@h-MBP-sl-myt.home>
2017-09-14 14:25:04 +0300

    complete design and gameplay with no relative layout

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+let backColor = "#34495e";
 let ROWS = 30;
 let COLS = 50;
 let snake = {
@@ -6,19 +6,52 @@ let snake = {
     'direction': 'r',
     'place': []
 }
-
-
+let addBlock = [];
+let fruit = {
+    placex: 0,
+    placey: 0
+};
+window.onkeydown = function (e) {
+    var code = e.keyCode ? e.keyCode : e.which;
+    switch (code) {
+        case 38:
+            if (snake.direction != 'd')
+                snake.direction = 'u';
+            //up keycode
+            break;
+        case 39:
+            if (snake.direction != 'l')
+                snake.direction = 'r';
+            //right keycode
+            break;
+        case 40:
+            if (snake.direction != 'u')
+                snake.direction = 'd';
+            //down
+            break;
+        case 37:
+            if (snake.direction != 'r')
+                snake.direction = 'l';
+            //left
+            break;
+    }
+};
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-async function demo() {
-    console.log('Taking a break...');
-    await sleep(1000);
-    console.log('one second later');
+
+
+
+let paintBack = function(){
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; ++j) {
+            var li = document.getElementById('u' + i + 'l' + j);
+            li.style.backgroundColor = backColor;
+        }
+    }
 }
 
-//demo();
 var initiateBoard = function () {
     let board = document.getElementById("board");
     for (let i = 0; i < ROWS; i++) {
@@ -49,49 +82,183 @@ var ceateSnake = function () {
         'x': 5,
         'y': 7,
     }
+    snake.size = 1;
     snake.place.push(square);
-
-    snake.place.push(square1);
-    snake.place.push(square2);
-
-
-    snake.size = 3;
-    snake.direction = 'r';
-
     drawSnake();
+    snake.size = 2;
+    snake.place.push(square1);
+    drawSnake();
+    snake.size = 3;
+    snake.place.push(square2);
+    drawSnake();
+    snake.direction = 'r';
 }
 
 let drawSnake = function () {
-    for (let i = 0; i < snake.size; ++i) {
-        let squareToColor = snake.place[i];
-        block = document.getElementById('u' + squareToColor.x + 'l' + squareToColor.y);
-        block.style.backgroundColor = "white";
-    }
+    let squareToColor = snake.place[snake.size - 1];
+    block = document.getElementById('u' + squareToColor.x + 'l' + squareToColor.y);
+    block.style.backgroundColor = "white";
 }
 
 
 let createFruit = function () {
     let placex = Math.floor(Math.random() * ROWS);
     let placey = Math.floor(Math.random() * COLS);
-    while (!noConflict(placex,placey)) {
+    while (!noConflictFruit(placex, placey)) {
         placex = Math.floor(Math.random() * ROWS);
         placey = Math.floor(Math.random() * COLS);
     }
     block = document.getElementById('u' + placex + 'l' + placey);
     block.style.backgroundColor = "orange";
+    fruit.placex = placex;
+    fruit.placey = placey;
 }
 
-let noConflict = function (x,y) {
-    for(let i=0;i<snake.size;++i){
-        if(snake.place[i].x == x && snake.place[i].y == y){
+let noConflictFruit = function (x, y) {
+    for (let i = 0; i < snake.size; ++i) {
+        if (snake.place[i].x == x && snake.place[i].y == y) {
             return false;
         }
     }
     return true;
 }
 
+async function startMovement() {
+    await sleep(100);
+    while (snakeStep()) {
+        await sleep(100);
+        if (hitFruit()) {
+            var place = {
+                x: fruit.placex,
+                y: fruit.placey
+            }
+            addBlock.push(place);
+            createFruit();
+        }
+    }
+    let endingtext = document.createElement("h1");
+    endingtext.innerText = "Congrats, your score is " + snake.size;
+    endingtext.setAttribute("id","h1");
+    document.body.appendChild(endingtext);
+    let endingbutton= document.createElement("button");
+    endingbutton.setAttribute("onclick","resetGame()");
+    endingbutton.innerText = "Play Again";
+    endingbutton.setAttribute("id","button");
+    document.body.appendChild(endingbutton);
+}
+
+let hitFruit = function () {
+    if (fruit.placex == snake.place[snake.size - 1].x && fruit.placey == snake.place[snake.size - 1].y) {
+        return true;
+    }
+    return false;
+}
+
+let noConflictNextStep = function () {
+    switch (snake.direction) {
+        case 'r':
+            var s = {
+                'x': snake.place[snake.size - 1].x,
+                'y': snake.place[snake.size - 1].y + 1
+            }
+            break;
+        case 'l':
+            var s = {
+                'x': snake.place[snake.size - 1].x,
+                'y': snake.place[snake.size - 1].y - 1
+            }
+            break;
+        case 'u':
+            var s = {
+                'x': snake.place[snake.size - 1].x - 1,
+                'y': snake.place[snake.size - 1].y
+            }
+            break;
+        case 'd':
+            var s = {
+                'x': snake.place[snake.size - 1].x + 1,
+                'y': snake.place[snake.size - 1].y
+            }
+            break;
+    }
+    if(s.x == ROWS || s.x == -1 || s.y == COLS || s.y == -1){
+        return false;
+    }
+    return true;
+}
+
+let snakeStep = function () {
+    switch (snake.direction) {
+        case 'r':
+            var s = {
+                'x': snake.place[snake.size - 1].x,
+                'y': snake.place[snake.size - 1].y + 1
+            }
+            break;
+        case 'l':
+            var s = {
+                'x': snake.place[snake.size - 1].x,
+                'y': snake.place[snake.size - 1].y - 1
+            }
+            break;
+        case 'u':
+            var s = {
+                'x': snake.place[snake.size - 1].x - 1,
+                'y': snake.place[snake.size - 1].y
+            }
+            break;
+        case 'd':
+            var s = {
+                'x': snake.place[snake.size - 1].x + 1,
+                'y': snake.place[snake.size - 1].y
+            }
+            break;
+    }
+    if(s.x == ROWS || s.x == -1 || s.y == COLS || s.y == -1){
+        return false;
+    }
+    snake.place.push(s);
+    if (!needToStay()) {
+        var li = document.getElementById('u' + snake.place[0].x + 'l' + snake.place[0].y);
+        li.style.background = backColor;
+        snake.place.splice(0, 1);
+    } else {
+        snake.size++;
+    }
+    drawSnake();
+    return true;
+}
+
+let needToStay = function () {
+    for (let i = 0; i < addBlock.length; ++i) {
+        if (snake.place[0].x == addBlock[i].x && snake.place[0].y == addBlock[i].y) {
+            addBlock.splice(i, 1);
+            return true;
+        }
+    }
+    return false;
+}
 
 
 initiateBoard();
 ceateSnake();
 createFruit();
+startMovement();
+
+
+let resetGame = function(){
+    var h = document.getElementById("h1");
+    var b = document.getElementById("button");
+    h.remove();
+    b.remove();
+    paintBack();
+    while(snake.place.length != 0){
+        snake.place.pop();
+    }
+    snake.size=0;
+    ceateSnake();
+    createFruit();
+    startMovement();
+}
+
+
